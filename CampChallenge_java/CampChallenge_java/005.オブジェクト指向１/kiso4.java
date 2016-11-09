0eba6bf9acd9df1c79ff87be2ae5b73de6aaca06
NAOKI-TAKAYAMA\Naoki <9asparagus@gmail.com>
2016-11-09 11:25:03 +0900

    基礎課題４のソースコード

---
+++ CampChallenge_java/CampChallenge_java/005.オブジェクト指向１/kiso4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Naoki
+ */
+public class kiso4 extends kiso3{
+    public int[] syoukyo(int[] r){
+        for(int i=0;i<r.length;i++){
+            r[i] = 0;
+        }
+        return r;
+    }
+}
+
